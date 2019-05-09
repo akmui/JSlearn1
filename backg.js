fc554a7c54b111e2b017faaae6c9f9a603e5441c
akmui <akmui0912@gmail.com>
2019-05-09 19:10:23 +0900

    revision

--- backg.js
+++ backg.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 4;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src =`/${imgNumber + 1}.jpg`;
+    image.src =`${imgNumber + 1}.jpg`;
     body.prepend(image);
     image.classList.add('bgImage');
 }
